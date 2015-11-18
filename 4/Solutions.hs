9bea42131af9ecc8584a0871d063d190f58e761f
Tony Li <tonyli.lives@gmail.com>
2015-11-17 22:21:40 -0800

    start assignment file

---
+++ 4/Solutions.hs
@@ -0,0 +1,12 @@
+{-
+   Byron Duenas
+   34095117
+   v5e8
+
+   Tongli Li
+   15688112
+   w6d8
+-}
+
+-- Notice: all functions must have explicit type declaration.
+-- Notice: all funcitons must have comments.
