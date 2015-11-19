2f824188b7afc9e698c83dc3dbb9c1f2e0fdec06
Tony Li <tonyli.lives@gmail.com>
2015-11-19 00:52:07 -0800

    use option 1

--- 4/Question1.hs
+++ 4/Question1.hs
@@ -11,19 +11,8 @@ kungPaoFactor
     -> Int
     -> Int
     -> Float
--- Option 1
--- kungPaoFactor r dm ds n c ft ff s =
---     term1 + term2
---     where
---         term1 = fromIntegral n / 30 - ds / dm
---         term2 = 10 * (fromIntegral s ** 2) * sqrt (fromIntegral r) / fromIntegral (c * (ft - ff + 1))
-
--- Option 2
 kungPaoFactor r dm ds n c ft ff s =
     term1 + term2
-        where
+    where
         term1 = fromIntegral n / 30 - ds / dm
-        term2 = 10 * stress / fromIntegral (c * food)
-            where
-            stress = (fromIntegral s ** 2) * sqrt (fromIntegral r)
-            food = ft - ff + 1
+        term2 = 10 * (fromIntegral s ** 2) * sqrt (fromIntegral r) / fromIntegral (c * (ft - ff + 1))
