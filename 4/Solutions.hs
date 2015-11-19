b48adcc6e97faf9718a7a121840d5230ac024346
Tony Li <tonyli.lives@gmail.com>
2015-11-19 13:31:23 -0800

    merge question 2 into solutions
    add comments

--- 4/Solutions.hs
+++ 4/Solutions.hs
@@ -34,3 +34,24 @@ kungPaoFactor r dm ds n c ft ff s =
         -- Use fromIntegral to convert an Int to a Float for calculation
         term1 = fromIntegral n / 30 - ds / dm
         term2 = 10 * (fromIntegral s ** 2) * sqrt (fromIntegral r) / fromIntegral (c * (ft - ff + 1))
+
+-- Question 2
+
+-- The natural recursion method calls the same function and builds on the call stack until the recursion terminates.
+harmonicNatural :: Int -> Float
+harmonicNatural n
+    | n == 1    = harm                              -- The base case
+    | otherwise = harm + harmonicNatural (n - 1)    -- The recursive step
+        where
+        harm = 1 / fromIntegral n
+
+-- The tail recursion method calls a helper function with an accumulator.
+harmonicTail :: Int -> Float
+harmonicTail n = harmonicHelper n 1 -- The accumulator has the result of the first harmonic sum
+
+-- Add the result of the previous sumations to the current n-th harmonic sequence to get the current sum.
+-- Then call recursively with the next decreasing integer in the sequence and the current sum (as the accumulator) until the recursion terminates.
+harmonicHelper :: Int -> Float -> Float
+harmonicHelper n total
+    | n == 1    = total                                                 -- The base case
+    | otherwise = harmonicHelper (n - 1) (total + 1 / fromIntegral n)   -- The recursive step
