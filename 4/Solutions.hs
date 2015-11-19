1c18e01720e3c7fee942bfbdd822fee2c0a0a5a5
Tony Li <tonyli.lives@gmail.com>
2015-11-19 13:19:31 -0800

    merge question 1 into solutions
    add comments

--- 4/Solutions.hs
+++ 4/Solutions.hs
@@ -9,4 +9,28 @@
 -}
 
 -- Notice: all functions must have explicit type declaration.
--- Notice: all funcitons must have comments.
+-- Notice: all functions must have comments.
+
+module Solutions
+    where
+
+-- Question 1
+
+kungPaoFactor
+    :: Int
+    -> Float
+    -> Float
+    -> Int
+    -> Int
+    -> Int
+    -> Int
+    -> Int
+    -> Float
+-- Calculate the Kung Pao Factor to determine whether or not to order Chinese take out.
+-- This formula is the sum of two terms.
+kungPaoFactor r dm ds n c ft ff s =
+    term1 + term2
+    where
+        -- Use fromIntegral to convert an Int to a Float for calculation
+        term1 = fromIntegral n / 30 - ds / dm
+        term2 = 10 * (fromIntegral s ** 2) * sqrt (fromIntegral r) / fromIntegral (c * (ft - ff + 1))
