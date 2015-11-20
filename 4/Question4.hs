a8451fad6f46a67a097e12f83b1f84a99959a978
Tony Li <tonyli.lives@gmail.com>
2015-11-19 23:02:01 -0800

    move the coins out to a variable and add comments

--- 4/Question4.hs
+++ 4/Question4.hs
@@ -1,3 +1,16 @@
+-- The set of all available coins to use.
+coins :: [Int]
+coins = [100,50,20,10,5,2,1]
+
+-- Compute the smallest set of coins to make up an amount.
 change :: Int -> [Int]
+-- The base case
 change 0 = []
-change total =[x |let coins = [y | y <- [100,50,20,10,5,2,1], y <= total], let biggest = head (coins), x <- (biggest:change(total - biggest))]
+-- The recursive step using list comprehension
+change total =
+    [
+        x |
+        let cs = [y | y <- coins, y <= total],      -- A list of coins that are each smaller than the current total
+        let biggest = head (cs),                    -- The current coin to add to the set of coins
+        x <- (biggest : change (total - biggest))   -- Generate a list of coins with the current coin and list of coins found by recursing with the remaining amount
+    ]
