eecce1ea3d16c646a00f20faa34748e73b81eb01
Tony Li <tonyli.lives@gmail.com>
2015-12-01 13:45:24 -0800

    shorten function

--- Project 2/Crusher.hs
+++ Project 2/Crusher.hs
@@ -183,13 +183,10 @@ crusher (current:old) p d n = -- To Be Completed
 
 gameOver :: Board -> [Board] -> Int -> Bool
 gameOver board history n
-    | board `elem` history      = True  -- Look for the current board inside the history of boards
-    | insufficientPieces 'W'    = True  -- Check if W has enough pieces
-    | insufficientPieces 'B'    = True  -- Check if B has enough pieces
-    | otherwise                 = False -- Game continues
-        where
-            insufficientPieces 'W' = length (filter (== W) board) < n   -- Case W has < n pieces
-            insufficientPieces 'B' = length (filter (== B) board) < n   -- Case B has < n pieces
+    | board `elem` history              = True  -- Look for the current board inside the history of boards
+    | length (filter (== W) board) < n  = True  -- Check if W has enough pieces
+    | length (filter (== B) board) < n  = True  -- Check if B has enough pieces
+    | otherwise                         = False -- Game continues
 
 --
 -- strToBoard
