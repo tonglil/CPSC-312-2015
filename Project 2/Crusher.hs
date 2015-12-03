b1efca3e5961d59cc1eb17097367599dd0b14a5e
Tony Li <tonyli.lives@gmail.com>
2015-12-02 19:43:58 -0800

    temp wip
    will swap form D->B

--- Project 2/Crusher.hs
+++ Project 2/Crusher.hs
@@ -535,9 +535,51 @@ jumpDown2Left1 b p n
 
 {-
    generateNewStates :: Board -> [Board] -> Grid -> [Slide] -> [Jump] -> Piece -> [Board]
-   generateNewStates board history grid slides jumps player = -- To Be Completed
+   generateNewStates board history grid slides jumps player -- To Be Completed
+       | null moves    = []
+       | otherwise     = (head moves)
+           where
+               state = boardToState board grid
+               moves = moveGenerator state slides jumps player
 -}
 
+applyMoves :: State -> [Move] -> Piece -> [State]
+applyMoves state moves player
+    | null moves    = []
+    | otherwise     = applyMove state (head moves) player : applyMoves state (tail moves) player
+
+{-
+   let state = [(W,(0,0)),(W,(1,0)),(W,(2,0)),(D,(0,1)),(W,(1,1)),(W,(2,1)),(D,(3,1)),(D,(0,2)),(D,(1,2)),(D,(2,2)),(D,(3,2)),(D,(4,2)),(D,(0,3)),(B,(1,3)),(B,(2,3)),(D,(3,3)),(B,(0,4)),(B,(1,4)),(B,(2,4))]
+   --           ^ ---------------------------> ^
+   let tile = (W,(0,0))  -- Start tile
+   let tile2 = (D,(0,1)) -- Dest tile
+   let tile3 = (D,(1,1)) -- Not part of move
+   let move = ((0,0),(0,1))
+   let piece = W
+-}
+
+-- For each tile in state, check if move will change it
+applyMove :: State -> Move -> Piece -> State
+applyMove state move player
+    | null state    = [] -- This case doesn't really make sense
+    | otherwise     = processTile (head state) move player : applyMove (tail state) move player
+
+-- Convert the tile if it is part of the move
+processTile :: Tile -> Move -> Piece -> Tile
+processTile tile move player
+    -- The src tile -> becomes blank tile
+    | fst tile == player && snd tile == fst move    = (D, snd tile)
+    -- The dest tile -> becomes new tile
+    | snd tile == snd move                          = (player, snd tile)
+    -- This tile is not a part of the move
+    | otherwise                                     = tile
+
+
+-- For each move
+--      Apply to state to create a new state
+-- For each new state
+--      Convert to board
+--      Filter out ones that have been played before (in history)
 
 
 
