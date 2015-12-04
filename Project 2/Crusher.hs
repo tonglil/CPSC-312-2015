be9ad311a2cb8a78acf5d18e3dcfec88c6b139b6
Tony Li <tonyli.lives@gmail.com>
2015-12-03 21:38:45 -0800

    cleanup comments

--- Project 2/Crusher.hs
+++ Project 2/Crusher.hs
@@ -266,16 +266,8 @@ boardToStr b = map (\ x -> check x) b
 --     (0,3),(1,3),(2,3),(3,3)
 --        (0,4),(1,4),(2,4)]
 --
--- Returns: the corresponding Grid i.e the acc when n3 == -1
+-- Note: This function on being passed 4 3 6 [] would produce
 --
-
---       [(0,0),(1,0),(2,0)
---     (0,1),(1,1),(2,1),(3,1)
---  (0,2),(1,2),(2,2),(3,2),(4,2)
---     (0,3),(1,3),(2,3),(3,3)
---        (0,4),(1,4),(2,4)()]
-
-
 --         [(0,0),(1,0),(2,0),(3,0),
 --       (0,1),(1,1),(2,1),(3,1),(4,1),
 --    (0,2),(1,2),(2,2),(3,2),(4,2),(5,2),
@@ -283,6 +275,9 @@ boardToStr b = map (\ x -> check x) b
 --    (0,4),(1,4),(2,4),(3,4),(4,4),(5,4),
 --      (0,5),(1,5),(2,5),(3,5),(4,5),
 --         (0,6),(1,6),(2,6),(3,6)]
+--
+-- Returns: the corresponding Grid i.e the acc when n3 == -1
+--
 
 generateGrid :: Int -> Int -> Int -> Grid -> Grid
 generateGrid n1 n2 n3 acc
